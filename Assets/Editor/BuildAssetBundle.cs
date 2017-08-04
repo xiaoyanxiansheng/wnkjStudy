eefa2076dcfedbae8c7c43bf4ab860f3d2abe086
xiaoyaoxiansheng <360507245@qq.com>
2017-08-04 18:21:41 +0800

    aa

---
+++ Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+public class BuildAssetBundle : MonoBehaviour {
+
+    [MenuItem("Custom Editor/Create AssetBundles Main")]
+    static void CreateAssetbundlesMain()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        AssetDatabase.Refresh();
+    }
+}
